97f41ed177ad92aba0eeda5bcdf8e6e7fd7f7870
Leo Lozach <leo.lozach@spikeelabs.fr>
2017-04-04 14:18:30 +0200

    Add tests

--- glob-ignore.js
+++ glob-ignore.js
@@ -5,9 +5,9 @@ module.exports = function (patterns, filters, options, callback) {
   callback = callback || options || filters
   options = typeof options === 'object' ? options : filters
 
-  if(typeof options !== 'object') options = {}
+  if (typeof options !== 'object') options = {}
   if (typeof filters === 'string') filters = [filters]
-  else if (!Array.isArray(filters)) filters = null
+  else filters = Array.isArray(filters) ? filters : null
 
   glob(patterns, options, function (err, paths) {
     if (err) return callback(err)
@@ -21,57 +21,18 @@ module.exports = function (patterns, filters, options, callback) {
   })
 }
 
-module.exports.sync = function(patterns, filters, options) {
+module.exports.sync = function (patterns, filters, options) {
   options = options || filters
 
+  if (typeof options !== 'object') options = {}
   if (typeof filters === 'string') filters = [filters]
-  else if (!Array.isArray(filters)) filters = null
+  else filters = Array.isArray(filters) ? filters : null
 
   var paths = glob.sync(patterns, options)
   if (filters) {
     var ig = ignore().add(filters)
     paths = ig.filter(paths)
   }
+
   return paths
 }
-
-/*
-module.exports('test/**', {}, {dot:false},function (err, res) {
-  if (err) throw err
-  console.log(res)
-})*/
-
-//console.log(module.exports.sync('test/**', {}, {dot: false}))
-
-/*const path = require('path')
-
- const glob = require('glob')
- const ignore = require('ignore')
- const ig = ignore().add(['.git'])
-
- var dir = path.join(__dirname, 'test')
-
-
- module.exports = function(dir, filters, callback) {
- callback = callback || filters
- if(typeof filters === 'string') filters = [filters]
- else if(!Array.isArray(filters)) filters = null
-
- glob('**', {cwd: dir, dot:true}, function(err, paths) {
- if(err) return callback(err)
-
- if(filters) {
- var ig = ignore().add(filters)
- paths = ig.filter(paths)
- }
-
- return callback(err, paths)
- })
- }
-
- module.exports('**', '.git', function(err, res) {
- if(err) throw err
- console.log(res)
- })
- */
-
--- test.js
+++ test.js
@@ -1,3 +1,98 @@
-/**
- * Created by Utilisateur on 04/04/2017.
- */
+/* eslint-env mocha */
+/* istanbul ignore next */
+const path = require('path')
+const assert = require('assert')
+const mock = require('mock-require')
+var glob = require('./glob-ignore.js')
+
+const cwd = path.join(__dirname, 'files')
+
+describe('glob-ignore (sync)', function () {
+  it('should get all the files', function (done) {
+    glob('**', {cwd: cwd}, function (err, res) {
+      if (err) return done(err)
+      assert.deepEqual(res, ['a.txt', 'b.txt', 'c.txt', 'x', 'x/y.txt', 'x/z.txt'])
+      done()
+    })
+  })
+
+  it('should filter files', function (done) {
+    glob('x/**', {cwd: cwd}, function (err, res) {
+      if (err) return done(err)
+      assert.deepEqual(res, ['x', 'x/y.txt', 'x/z.txt'])
+      done()
+    })
+  })
+
+  it('should worked without filters', function (done) {
+    glob('files/**', function (err, res) {
+      if (err) return done(err)
+      assert.deepEqual(res, ['files', 'files/a.txt', 'files/b.txt', 'files/c.txt', 'files/x', 'files/x/y.txt', 'files/x/z.txt'])
+      done()
+    })
+  })
+
+  it('should ignore a wrong filter', function (done) {
+    glob('**', {wrong: 'filter'}, {cwd: cwd}, function (err, res) {
+      if (err) return done(err)
+      assert.deepEqual(res, ['a.txt', 'b.txt', 'c.txt', 'x', 'x/y.txt', 'x/z.txt'])
+      done()
+    })
+  })
+
+  it('should worked with a string filter', function (done) {
+    glob('**', 'x', {cwd: cwd}, function (err, res) {
+      if (err) return done(err)
+      assert.deepEqual(res, ['a.txt', 'b.txt', 'c.txt'])
+      done()
+    })
+  })
+
+  it('should worked with multiple filters', function (done) {
+    glob('**', ['a.txt', 'x'], {cwd: cwd}, function (err, res) {
+      if (err) return done(err)
+      assert.deepEqual(res, ['b.txt', 'c.txt'])
+      done()
+    })
+  })
+
+  it('should capture glob-all errors', function (done) {
+    mock('glob-all', function (pattern, options, callback) {
+      callback(new Error('test'))
+    })
+    glob = mock.reRequire('./glob-ignore.js')
+    glob('**', {cwd: cwd}, function (err, res) {
+      assert.equal(err.message, 'test')
+      assert.equal(res, undefined)
+      mock.stop('glob-all')
+      glob = mock.reRequire('./glob-ignore.js')
+      done()
+    })
+  })
+})
+
+describe('glob-ignore (sync)', function () {
+  it('should get all the files', function () {
+    assert.deepEqual(glob.sync('**', {cwd: cwd}), ['a.txt', 'b.txt', 'c.txt', 'x', 'x/y.txt', 'x/z.txt'])
+  })
+
+  it('should filter files', function () {
+    assert.deepEqual(glob.sync('x/**', {cwd: cwd}), ['x', 'x/y.txt', 'x/z.txt'])
+  })
+
+  it('should work without filters', function () {
+    assert.deepEqual(glob.sync('files/**'), ['files', 'files/a.txt', 'files/b.txt', 'files/c.txt', 'files/x', 'files/x/y.txt', 'files/x/z.txt'])
+  })
+
+  it('should ignore a wrong filter', function () {
+    assert.deepEqual(glob.sync('**', {wrong: 'filter'}, {cwd: cwd}), ['a.txt', 'b.txt', 'c.txt', 'x', 'x/y.txt', 'x/z.txt'])
+  })
+
+  it('should worked with a string filter', function () {
+    assert.deepEqual(glob.sync('**', 'x', {cwd: cwd}), ['a.txt', 'b.txt', 'c.txt'])
+  })
+
+  it('should worked with multiple filters', function () {
+    assert.deepEqual(glob.sync('**', ['a.txt', 'x'], {cwd: cwd}), ['b.txt', 'c.txt'])
+  })
+})
